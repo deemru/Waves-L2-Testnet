c971aa348034fab3f2d630e6823b001161fd5a70
Dmitrii Pichulin <deem@deem.ru>
2024-05-20 16:16:23 +0300

    better waves logging

--- run-detached.sh
+++ run-detached.sh
@@ -4,6 +4,8 @@ REPO_DIR=$(dirname $0)
 cd $REPO_DIR
 WORK_DIR=work
 cd $WORK_DIR
-chown -R 1000 ./besu
-chown -R 1000 ./waves
+touch ./waves/waves.log
+chmod a+w ./waves/waves.log
+chown -R 1000:1000 ./besu
+chown -R 1000:1000 ./waves
 docker compose up -d
