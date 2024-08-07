4722695196b021dd99653b89088644c3c8ff6d42
Dmitrii Pichulin <deem@deem.ru>
2024-08-08 00:59:38 +0300

    cleanup code and downloaded data

--- setup.sh
+++ setup.sh
@@ -19,11 +19,28 @@ mkdir -p ./log/besu
 mkdir -p ./log/waves
 
 # WAVES DATA
-[ -d ./waves/data/tx-meta ] || { echo "WARNING: No blockchain data found."; echo "WARNING: Blockchain data will be downloaded."; echo "Press any key to continue... "; read -s -r -n 1; echo "Downloading..."; wget -qO- --show-progress https://blockchain-testnet.wavesnodes.com/blockchain_last.tar | tar xv -C ./waves/data; }
+[ -d ./waves/data/tx-meta ] || {
+    echo "WARNING: No blockchain data found.";
+    echo "WARNING: Blockchain data will be downloaded.";
+    echo "Press any key to continue... ";
+    read -s -r -n 1; echo "Downloading...";
+    wget -qO- --show-progress https://blockchain-testnet.wavesnodes.com/blockchain_last.tar | tar xv -C ./waves/data;
+}
 [ -d ./waves/data/tx-meta ] || { echo "ERROR: Still no blockchain data found."; exit 1; }
 
 # BESU DATA
-[ -f ./besu/database/LOG ] || { echo "WARNING: No blockchain L2 data found."; echo "WARNING: Blockchain L2 data will be downloaded."; echo "Downloading..."; wget -qO- --show-progress https://blockchain.unit0.dev/l2-bc-latest.tar | tar xv -C ./besu; }
+[ -f ./besu/database/LOG ] || {
+    echo "WARNING: No blockchain L2 data found.";
+    echo "WARNING: Blockchain L2 data will be downloaded.";
+    echo "Downloading...";
+    mkdir -p ./temp;
+    rm -rf ./temp/*;
+    wget -qO- --show-progress https://blockchain.unit0.dev/l2-bc-latest.tar | tar xv -C ./temp;
+    rm -rf ./besu/database;
+    mv -f ./temp/database ./besu/;
+    mv -f ./temp/DATABASE_METADATA.json ./besu/;
+    rm -rf ./temp;
+}
 [ -f ./besu/database/LOG ] || { echo "ERROR: Still no blockchain L2 data found."; exit 1; }
 
 cp ../support/docker-compose.yml ./docker-compose.yml;
